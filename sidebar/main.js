7667f13e9653fa9f62960e1726f736ceca98ef5d
giulliano <giullianomendes033@gmail.com>
2022-03-23 19:17:23 -0300

    adicionado projeto 5

---
+++ sidebar/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.sidebar-toggle').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('sidebar-open');
+});
+
+
+document.querySelector('.btn-close').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('sidebar-open');
+});
